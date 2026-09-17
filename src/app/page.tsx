5027443f6a523920ecd2862845fd5e029ec11b59
Reinaldo <gamersolitavi4l@gmail.com>
2026-09-17 16:57:19 -0300

    feat: adicionando o componente shadcn de botao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-  <h1>codelab</h1>
+  <div>
+    <h1>codelab</h1>
+  <Button>Click me</Button>
+  </div>
   );
 }
