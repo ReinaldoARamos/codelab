589d809e766548d954d1a16243892fd6e91e30fa
Reinaldo <gamersolitavi4l@gmail.com>
2026-09-17 16:37:43 -0300

    feat: instalando o shadcn e colocando tema dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+
 import "@/styles/globals.css";
-import { cn } from "@/lib/util";
+import { cn } from "@/lib/utils";
+
 
 const nunito = Nunito({
   variable: "--font-sans",
   subsets: ["latin"],
 });
 
-
-
 export const metadata: Metadata = {
   title: "codelab",
-
 };
 
 export default function RootLayout({
@@ -21,10 +20,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" suppressHydrationWarning>
-      <body
-        className={cn(nunito.variable,  "antialiased font-sans")}
-      >
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={cn(nunito.variable, "antialiased font-sans dark" )}>
+        <h1>LAYOUT</h1>
         {children}
       </body>
     </html>
